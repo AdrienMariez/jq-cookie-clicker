a2d54a834a172ca7407182517814d167a5a290be
AdrienMariez <33837157+AdrienMariez@users.noreply.github.com>
2017-12-18 13:37:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 	//votre code
+var value_cookies = 0 ;
+
+	$('.cake-is-a-lie').click(function(){
+		value_cookies ++;
 
 
+		$(".counter").html(value_cookies);
+
+		if ( value_cookies == 5 )
+  			{
+  				alert('NaN');
+  			}
+
+	});
 });
